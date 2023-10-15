dd860340112e57397f798ae73121c3450695fb83
Alan Truong <alntruong@ucdavis.edu>
2023-10-14 20:08:51 -0700

    delete form.tsx

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Calendar from '../../components/Calendar/Calendar';
-import Form from '../../components/Form';
+
 import '../../components/Calendar/calendar.css';
 
 export default function Booking() {
